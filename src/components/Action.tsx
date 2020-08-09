6d17cf6f08a1c160d787a9048d238dcd51d2f038
Chris Ng <46430248+chris1324@users.noreply.github.com>
2020-08-09 11:44:17 +0800

    Complete

--- src/components/Action.tsx
+++ src/components/Action.tsx
@@ -1,19 +1,21 @@
-import React, { FunctionComponent } from 'react';
+import React from 'react';
 
 interface IProps {
-    handlePick: () => void;
-    hasOptions: boolean;
+	handlePick: () => void;
+	hasOptions: boolean;
 }
 
-const Action: FunctionComponent<IProps> = ({ handlePick, hasOptions }) => (
-    <div>
-        <button
-            className="big-button"
-            onClick={handlePick}
-            disabled={!hasOptions}
-        >
-            What should i do?</button>
-    </div>
+const Action: React.FC<IProps> = ({ handlePick, hasOptions }) => (
+	<div>
+		<button
+			className="big-button"
+			disabled={!hasOptions}
+			type="button"
+			onClick={handlePick}
+		>
+			What should i do?
+		</button>
+	</div>
 );
 
 export default Action;
--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -1,35 +1,38 @@
-import React, { FunctionComponent } from 'react';
+import React from 'react';
+// eslint-disable-next-line sort-imports
 import Option from './Option';
 
 interface IProps {
-    options: Array<string>;
-    handleDeleteOption: (option: string) => void;
-    handleDeleteOptions: () => void;
+	options: Array<string>;
+	handleDeleteOption: (option: string) => void;
+	handleDeleteOptions: () => void;
 }
 
-const Options: FunctionComponent<IProps> = ({ options, handleDeleteOption, handleDeleteOptions }) => (
-    <div>
-        <div className="widget-header">
-            <h3 className="widget-header__title">Your Options</h3>
-            <button
-                className="button button--link"
-                onClick={handleDeleteOptions}
-            >
-                Remove all
-        </button>
-        </div>
+const Options: React.FC<IProps> = ({ options, handleDeleteOption, handleDeleteOptions }) => (
+	<div>
+		<div className="widget-header">
+			<h3 className="widget-header__title">Your Options</h3>
+			<button
+				className="button button--link"
+				type="button"
+				onClick={handleDeleteOptions}
+			>
+				Remove all
+			</button>
+		</div>
 
-        {options.length === 0 && <p className="widget__message">Please add an option to get started!</p>}
-        <div>
-            {options.map((x, index) =>
-                <Option
-                    key={index}
-                    optionText={x}
-                    count={index + 1}
-                    handleDeleteOption={handleDeleteOption}
-                />)}
-        </div>
-    </div>
+		{options.length === 0 && <p className="widget__message">Please add an option to get started!</p>}
+		<div>
+			{options.map((x, index) => (
+				<Option
+					key={index}
+					count={index + 1}
+					handleDeleteOption={handleDeleteOption}
+					optionText={x}
+				/>
+			))}
+		</div>
+	</div>
 );
 
 export default Options;
