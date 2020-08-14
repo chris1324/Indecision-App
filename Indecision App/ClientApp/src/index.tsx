24831da3a6f5a332a250bea1ac27ca9edbdff081
chris1324 <46430248+chris1324@users.noreply.github.com>
2020-08-14 18:02:09 +0800

    Merge PR : Restructure Project (#16)

--- Indecision App/ClientApp/src/index.tsx
+++ Indecision App/ClientApp/src/index.tsx
@@ -5,7 +5,6 @@ import 'normalize.css/normalize.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'devextreme/dist/css/dx.common.css';
 import 'devextreme/dist/css/dx.light.css';
-import './styles/styles.scss';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
---
+++ Indecision App/ClientApp/src/scenes/Indecision/index.ts
@@ -0,0 +1,4 @@
+import './styles.scss';
+import Page from './components/Page';
+
+export default Page;
